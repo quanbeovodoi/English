6243b02af621829953c86e3399bd2b858c56d9d4
Quanbodoi <quanbeovodoi@gmail.com>
2023-05-12 15:56:36 +0700

    update head

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -4,36 +4,41 @@ import Navbar from '~/components/Navbar';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faBell, faCircleInfo, faEarthAsia } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(style);
 function Header() {
   
   return (
     <div className={cx('heading')}>
       <div className={cx('wrapper','top-heading')}>
-        <ul className={cx('navbar-links')}>
-          <li><h5 className={cx('heading')}>Welcome to QSHOP</h5></li>
-          <li><div className={cx('icon')}><FontAwesomeIcon icon={faFacebook}/></div></li>
-          <li><div className={cx('icon')}><FontAwesomeIcon icon={faTwitter}/></div></li>
-          <li><div className={cx('icon')}><FontAwesomeIcon icon={faYoutube}/></div></li>
-        </ul>
-        <ul className={cx('navbar-links')}>
-          <li className={cx('item')}>
-            <Link to={'/'}>Notification</Link>
-          </li>
-          <li className={cx('item')}>
-            <Link to={'/'}>Support</Link>
-          </li>
-          <li className={cx('item')}>
-            <Link to={'/'}>English</Link>
-          </li>
-        </ul>
+        <div className={cx('container')}>
+          <ul className={cx('navbar-links')}>
+            <li><h5 className={cx('heading')}>Welcome to QSHOP</h5></li>
+            <li><div className={cx('icon')}><FontAwesomeIcon icon={faFacebook}/></div></li>
+            <li><div className={cx('icon')}><FontAwesomeIcon icon={faTwitter}/></div></li>
+            <li><div className={cx('icon')}><FontAwesomeIcon icon={faYoutube}/></div></li>
+          </ul>
+          <ul className={cx('navbar-links')}>
+            <li className={cx('item')}>
+              <Link to={'/'}><span><FontAwesomeIcon icon={faBell} /></span>Notification</Link>
+            </li>
+            <li className={cx('item')}>
+              <Link to={'/'}><span><FontAwesomeIcon icon={faCircleInfo} /></span>Support</Link>
+            </li>
+            <li className={cx('item')}>
+              <Link to={'/'}><span><FontAwesomeIcon icon={faEarthAsia} /></span>English</Link>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className={cx('wrapper')}>
-        <div className={cx('image')}>
-          <img src={require("../../assets/Images/kingback-website-favicon-black.png")}></img>
-          <h1 className={cx('title')}><span>Q</span>SHOP</h1>
+        <div className={cx('container')}>
+          <div className={cx('image')}>
+            <img src={require("../../assets/Images/kingback-website-favicon-black.png")}></img>
+            <h1 className={cx('title')}><span>Q</span>SHOP</h1>
+          </div>
+          <Navbar/>
         </div>
-        <Navbar/>
       </div>
     </div>
   );
